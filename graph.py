025de463ee54697d2a762e0e913e5c45fac495ba
tommy-mor <thmorriss@gmail.com>
2024-02-10 12:51:40 -0500

    shows sum of health over time

--- graph.py
+++ graph.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-players = []
+
+redplayers = []
+bluplayers = []
 
 with open('./file.txt', 'r') as f:
     f.readline()
@@ -13,8 +15,13 @@ with open('./file.txt', 'r') as f:
     while True:
         string = f.readline().strip()
 
-        if 'Player: ' in string:
-            players.append([ticks, healths])
+        if 'Player:' in string:
+            [_, team, player] = string.split(':')
+            if team == 'Red':
+                redplayers.append([ticks, healths])
+            else:
+                bluplayers.append([ticks, healths])
+
             ticks = list()
             healths = list()
         elif string == '':
@@ -29,9 +36,30 @@ with open('./file.txt', 'r') as f:
 
 import matplotlib.pyplot as plt
 
-for [ticks, healths] in players:
-    plt.plot(ticks, healths)
+max_length = 99999999999
+for [ticks, healths] in bluplayers:
+    max_length = min(max_length, ticks[-1])
 
+for [ticks, healths] in redplayers:
+    max_length = min(max_length, ticks[-1])
+
+print(max_length)
+data = np.ndarray(shape=(2, 6, max_length))
+
+
+for i, [ticks, healths] in enumerate(bluplayers):
+    # set plt color 
+
+    #plt.plot(healths, color='blue')
+    data[0][i] = np.array(healths)[:max_length]
+
+for i, [ticks, healths] in enumerate(redplayers):
+    #plt.plot(healths, color='red')
+    data[1][i] = np.array(healths)[:max_length]
+
+
+plt.plot(data[0].sum(axis=0), color='blue')
+plt.plot(data[1].sum(axis=0), color='red')
 plt.xlabel('Tick')
 plt.ylabel('Health')
 plt.title('Health over time')
